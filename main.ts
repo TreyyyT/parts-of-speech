532d9b8466f336492688edd83f2a483b246e1403
TreyyyT <82166923+TreyyyT@users.noreply.github.com>
2021-04-22 12:18:09 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,116 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    duck.say(sentence)
+    cat.say(nouns)
+    bat.say(verbs)
+    monkey.say(adjectives)
+    snake.say(pronouns)
+})
+let pronouns = ""
+let adjectives = ""
+let verbs = ""
+let nouns = ""
+let sentence = ""
+let snake: Sprite = null
+let monkey: Sprite = null
+let bat: Sprite = null
+let cat: Sprite = null
+let duck: Sprite = null
+scene.setBackgroundColor(9)
+duck = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+cat = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+bat = sprites.create(img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c 1 1 b b b 1 1 b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, SpriteKind.Player)
+monkey = sprites.create(img`
+    . . . . . f f f f f . . . . . . 
+    . . . . f e e e e e f . . . . . 
+    . . . f d d d d d e e f . . . . 
+    . . f f f d d f f d e f f . . . 
+    . c d d e e d d d d e d d f . . 
+    . c c d d d d c d d e d f f f . 
+    . c d c c c c d d d e d f b d f 
+    . . c d d d d d d e e f f d d f 
+    . . . c d d d d e e f f e f f f 
+    . . . . f f f e e f e e e f . . 
+    . . . . f e e e e e e e f f f . 
+    . . . f e e e e e e f f f e f . 
+    . . f f e e e e f f f f f e f . 
+    . f b d f e e f b b f f f e f . 
+    . f d d f e e f d d b f f f f . 
+    . f f f f f f f f f f f f f . . 
+    `, SpriteKind.Player)
+snake = sprites.create(img`
+    . . . . . c c c c c c c . . . . 
+    . . . . c 6 7 7 7 7 7 6 c . . . 
+    . . . c 7 c 6 6 6 6 c 7 6 c . . 
+    . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+    . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+    . . f 7 8 1 f f 1 6 7 7 7 f . . 
+    . . f 6 f 1 f f 1 f 7 7 7 f . . 
+    . . . f f 2 2 2 2 f 7 7 6 f . . 
+    . . c c f 2 2 2 2 7 7 6 f c . . 
+    . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+    c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+    f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+    f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+    . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+    . . f f c c c c c c c c . . . . 
+    `, SpriteKind.Player)
+duck.setPosition(80, 10)
+cat.setPosition(80, 100)
+bat.setPosition(80, 60)
+monkey.setPosition(30, 75)
+snake.setPosition(130, 75)
+sentence = "I am a duck. Please don't hurt my yellow body."
+nouns = "" + sentence.substr(7, 4) + sentence.substr(40, 5)
+verbs = "" + sentence.substr(2, 2) + ("" + sentence.substr(19, 6) + sentence.substr(25, 5))
+adjectives = sentence.substr(34, 6)
+pronouns = "" + sentence.substr(0, 1) + sentence.substr(30, 3)
